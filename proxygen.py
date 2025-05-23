28fc04dcf80d65f9b4b5bffb0ca0404fe05d1e84
R00tUs3r <sudo3r@proton.me>
2025-05-23 14:48:06 +0330

    modified:   proxygen.py

--- proxygen.py
+++ proxygen.py
@@ -131,24 +131,16 @@ class ProxyCollector:
         self._working = 0
         async with aiohttp.ClientSession() as session:
             semaphore = asyncio.Semaphore(self.concurrency)
-            async def bounded_check(proxy):
+            async def check_and_yield(proxy):
                 async with semaphore:
                     is_working = await self._check_proxy(session, proxy)
                     if is_working:
-                        yield proxy
-            tasks = []
-            for proxy in proxies:
-                tasks.append(self._check_and_yield(session, proxy, semaphore))
-            # gather returns a list of async generators, so we need to iterate over them
-            for coro in asyncio.as_completed(tasks):
-                async for working_proxy in await coro:
-                    yield working_proxy
-
-    async def _check_and_yield(self, session, proxy, semaphore):
-        async with semaphore:
-            is_working = await self._check_proxy(session, proxy)
-            if is_working:
-                yield proxy
+                        return proxy
+            tasks = [asyncio.create_task(check_and_yield(proxy)) for proxy in proxies]
+            for task in asyncio.as_completed(tasks):
+                proxy = await task
+                if proxy:
+                    yield proxy
 
 # Example usage:
 # async def main():
